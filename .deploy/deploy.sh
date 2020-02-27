417859833bb647511818eb37529f3f1c25f8521c
Steven Gliebe <3372396+stevengliebe@users.noreply.github.com>
2020-02-27 07:46:38 -0600

    Fix typo

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -2,6 +2,6 @@
 
 # Push repo to GitHub for hosting on GitHub Pages.
 # stevengliebe.com CNAME: stevenstevengliebe.github.io
-# Deploy script alias: deploy-sh
+# Deploy script alias: deploy-sg
 
 git push origin master
