4d5e9cc8fd84a1fd60426433330b2b50804432ef
Steven Gliebe <3372396+stevengliebe@users.noreply.github.com>
2020-03-03 10:46:16 -0600

    Rename .deploy to .dev

--- .dev/deploy.sh
+++ .dev/deploy.sh
@@ -2,6 +2,6 @@
 
 # Push repo to GitHub for hosting on GitHub Pages.
 # stevengliebe.com CNAME: stevenstevengliebe.github.io
-# Deploy script alias: deploy-sg
+# Deploy script alias: sg-deploy
 
 git push origin master
